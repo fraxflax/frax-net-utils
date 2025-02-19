1124d6fd8ed9a5ea47392c5911a3764c736db8a6
fraxflax <frax@axnet.nu>
2025-02-19 15:16:09 +0100

    reorg

--- install-frax-net-utils.sh
+++ install-frax-net-utils.sh
@@ -8,7 +8,7 @@ done
 printf 'Installing frax-net-utils...\n'
 for x in update-frax-net-utils.sh uninstall-frax-net-utils.sh pingarp pingsweep; do
     printf '* %s ... ' "$x"
-    curl -fso /usr/local/bin/$x https://raw.githubusercontent.com/fraxflax/frax-net-utils/refs/heads/main/$x >/dev/null 2>&1 || {
+    curl -fso /usr/local/bin/$x https://raw.githubusercontent.com/fraxflax/frax-net-utils/refs/heads/main/bin/$x >/dev/null 2>&1 || {
 	printf 'download failed!\n'
 	continue
     }
